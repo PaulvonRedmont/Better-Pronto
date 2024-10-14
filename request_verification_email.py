6cac3e33661b708a33467fc73a64eb40151e6c8a
S451 <146851640+Society451@users.noreply.github.com>
2024-10-14 09:32:34 -0500

    Update and rename send_verification_email.py to request_verification_email.py

--- request_verification_email.py
+++ request_verification_email.py
@@ -2,27 +2,30 @@ import requests, time
 
 class BackendError(Exception):
     pass
-#make some error handling thingies
 
 def post_user_verify(email):
     url = "https://accounts.pronto.io/api/v1/user.verify"
     payload = {"email": email}
     try:
-        start_time = time.time()
         response = requests.post(url, json=payload)
         response.raise_for_status()
         return response.json()
-        end_time = time.time()
-        total_time = end_time - start_time
-        print(f"Request sent in {total_time} seconds")
     except requests.exceptions.HTTPError as http_err:
         raise BackendError(f"HTTP error occurred: {http_err}")
     except Exception as err:
         raise BackendError(f"An error occurred: {err}")
+
 # Example Usage
 email = "example@ohs.stanford.edu"
+
 try:
+    print("Requesting verification code for", email)
+    request_start_time = time.time()
     result = post_user_verify(email)
+    request_end_time = time.time()
+    total_time = request_end_time - request_start_time
+    print(f"Request took {total_time:.2f} seconds.")
     print("Verification email sent:", result)
+    print(f"Please check {email} for the verification code.")
 except BackendError as e:
     print(e)
